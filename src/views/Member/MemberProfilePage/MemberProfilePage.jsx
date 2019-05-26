70e005c7fa156bb9ad246bd85fba54d7cbeef164
ebedev31 <ebedev31@gmail.com>
2019-05-26 21:36:21 +0200

    Sorted code with orignal implementation & new data

--- src/views/Member/MemberProfilePage/MemberProfilePage.jsx
+++ src/views/Member/MemberProfilePage/MemberProfilePage.jsx
@@ -11,20 +11,20 @@ import Palette from "@material-ui/icons/Palette";
 // core components
 import GridContainer from "components/Grid/GridContainer.jsx";
 import GridItem from "components/Grid/GridItem.jsx";
-//import NavPills from "components/NavPills/NavPills.jsx";
+import NavPills from "components/NavPills/NavPills.jsx";
 import Clearfix from "components/Clearfix/Clearfix.jsx";
 
 
 // App resources
 import memberPageStyle from "assets/member/jss/views/memberPageStyle.jsx";
-//import SDGGrid from "../SharedSection/SDGGrid";
-//import MemberFooter from "../SharedSection/MemberFooter.jsx";
+import SDGGrid from "../SharedSection/SDGGrid";
+import MemberFooter from "../SharedSection/MemberFooter.jsx";
 
 import MemberTeamSection from "./Sections/MemberTeamSection.jsx";
 import MemberHeaderSection from "./Sections/MemberHeaderSection.jsx";
-//import MemberTagsSection from "./Sections/MemberTagsSection.jsx";
+import MemberTagsSection from "./Sections/MemberTagsSection.jsx";
 import MemberBlogSection from "./Sections/MemberBlogSection.jsx";
-//import MemberSolutionSection from "./Sections/MemberSolutionSection.jsx";
+import MemberSolutionSection from "./Sections/MemberSolutionSection.jsx";
 import MemberSummarySection from "./Sections/MemberSummarySection.jsx";
 import MemberProjectSection from "./Sections/MemberProjectSection.jsx";
 import MemberFooterSection from "./Sections/MemberFooterSection.jsx";
@@ -43,18 +43,18 @@ class MemberProfilePage extends React.Component {
       classes.imgFluid
     );
 
-    const  member  = this.props.location.state.item;
+    const member  = this.props.location.state.item; 
     return (
       <div>
 
-        <MemberHeaderSection  bgImg={member.image.medium}/>
+        <MemberHeaderSection />
         <div className={classNames(classes.main, classes.mainRaised)}>
           <div className={classes.container}>
             <GridContainer justify="center">
               <GridItem xs={12} sm={12} md={6}>
                 <div className={classes.memberLogo}>
                   <div>
-                   
+                    <img src={member.image.medium} alt="logo" className={imageClasses} />
                     
                   </div>
                   <div className={classes.name}>
@@ -73,14 +73,71 @@ class MemberProfilePage extends React.Component {
                     {member.summary}
                   </p>
                 </div>
+
+                <SDGGrid sdg={member.sustainable_development_goals}/>
+
+              </GridItem>
+
+              <GridItem xs={12} sm={12} md={2}>
+
+                <MemberTagsSection member={member}/>
+
               </GridItem>
             </GridContainer>
-                   
+
+          {/**Condition for rendering to be put around nav pils.. 
+          should only show for acando */}
+
+            <NavPills
+                alignCenter
+                color="primary"
+                tabs={[
+                  {
+                    tabButton: "Summary",
+                    tabIcon: Palette,
+                    tabContent: (
+                      <MemberSummarySection />
+                    )
+                  },
+                  {
+                    tabButton: "Solutions",
+                    tabIcon: Palette,
+                    tabContent: (
+                      <MemberSolutionSection />
+                    )
+                  },
+                  {
+                    tabButton: "Projects",
+                    tabIcon: Palette,
+                    tabContent: (
+                      <MemberProjectSection />
+                    )
+                  },
+                  {
+                    tabButton: "Publications",
+                    tabIcon: Palette,
+                    tabContent: (
+                      <MemberBlogSection />
+                    )
+                  },
+                  {
+                    tabButton: "Datasets",
+                    tabIcon: Palette,
+                    tabContent: (
+                      <MemberBlogSection />
+                    )
+                  }
+                ]}
+              />                  
+
+
+
+            <MemberTeamSection />
+            <MemberFooterSection member={member} />        
             <Clearfix />
           </div>
-    </div>
-      
-      
+        </div> 
+        <MemberFooter />
       </div>
       
     );
