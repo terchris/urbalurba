f56741f25e3b13d15d5cf48cf3e5ad900f482528
ebedev31 <ebedev31@gmail.com>
2019-06-08 02:22:22 +0200

    fixed bugs arose from prev commit(orgtype img)

--- src/views/Member/MemberLandingPage/MemberLandingPage.jsx
+++ src/views/Member/MemberLandingPage/MemberLandingPage.jsx
@@ -13,6 +13,9 @@ import MemberFooter from "views/Member/SharedSection/MemberFooter.jsx";
 import MemberTop from "views/Member/SharedSection/MemberTop.jsx";
 import MemberGrid from "views/Member/SharedSection/MemberGrid.jsx";
 
+//image for background
+import bgImgOriginal from "assets/img/bg12.jpg";
+
 
 class MemberLandingPage extends React.Component {
     componentDidMount() {
@@ -27,6 +30,8 @@ class MemberLandingPage extends React.Component {
         let segmentTag = ""
         let sdgTag = ""
         let challengesTag = ""
+        let backgroundImage;
+        let orgTypeName;
 
         /**
          * Check to make sure which User case is being used 
@@ -38,7 +43,20 @@ class MemberLandingPage extends React.Component {
             segmentTag = this.props.location.state.segmentTag;
             sdgTag = this.props.location.state.sdgTag;
             challengesTag = this.props.location.state.challengesTag;
+            
+            //choose image source as well as orgtype string source
+            if(this.props.location.state.item)
+            {
+                
+                 backgroundImage= orgType.images
+                orgTypeName = orgType.name
+            }
+            else{
+                backgroundImage=bgImgOriginal
+                orgTypeName=""
+            }
         }
+        
         const { classes, ...rest } = this.props;
         const imageClasses = classNames(
             classes.imgRaised,
@@ -47,8 +65,8 @@ class MemberLandingPage extends React.Component {
         const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
         return (
             <div>
-                <MemberTop bgImg={orgType.images} />
-                <MemberGrid organization={orgType.name} segmentTag={segmentTag} challengesTag={challengesTag} sdgTag={sdgTag} />
+                <MemberTop bgImg={backgroundImage} />
+                <MemberGrid organization={orgTypeName} segmentTag={segmentTag} challengesTag={challengesTag} sdgTag={sdgTag} />
                 <MemberFooter />
             </div>
         );
